c9d13d4c6667bce297d205d32e07cf2fa18fcf80
Alanxzg <alangamarro0@gmail.com>
2025-08-18 23:19:59 -0600

    Create validar.php

---
+++ validar.php
@@ -0,0 +1,17 @@
+<?php
+include("conexion.php");
+$usuario = $_POST ['usuario'];
+$clave = $_POST  ['contraseña'];
+$sql = "SELECT * FROM user  WHERE usuario = '$usuario' AND  clave = '$clave'";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result)>0){
+
+    header("location:bienvenido.php");
+    exit();
+
+}else{
+    echo "USUARIO Y CONTRASEÑA INCORRECTOS";
+}
+?>
