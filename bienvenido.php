22bf3585264876981d174deaddf26794250a0496
Alanxzg <alangamarro0@gmail.com>
2025-08-18 23:19:01 -0600

    Update and rename README.md to bienvenido.php

---
+++ bienvenido.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bienvenido</title>
+</head>
+<body>
+    <h1>Bienvenido</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, vel, odio commodi inventore tenetur perferendis ea id dolore culpa voluptas quis dolorum, cumque vitae! Commodi vel ratione labore nostrum iusto.
+        
+    </p>
+</body>
+</html>
